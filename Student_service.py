8cdf4f47d175e70595a26655c810b8a6409e564a
Sandeep K <sandeepkumaresan4676@gmail.com>
2024-07-22 23:06:18 +0530

    studentservice

--- Student_service.py
+++ Student_service.py
@@ -2,3 +2,11 @@ from dbconfig import dbconfig
 from Student import user
 import json
 
+class user_service:
+    def get_users(self):
+        conn=dbconfig.openconnection()
+        cur=conn.cursor()
+        cur.execute("select * from student")
+        out=cur.fetchall()
+        for i in out:
+            print(i)
